f65953369d026c09662466565b535c1e72e6d5d4
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-03-31 20:18:27 +0600

    Sun Mar 31 08:18:27 PM +06 2024

---
+++ single_number.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  int singleNumber(vector<int> &nums) {
+    int result = 0;
+    for (int num : nums) {
+      result ^= num;
+    }
+    return result;
+  }
+};
