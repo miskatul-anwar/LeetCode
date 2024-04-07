ba4cc6d0e9b131a150f6913fe702919ff98d1764
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-04-07 21:46:14 +0600

    Sun Apr  7 09:46:14 PM +06 2024

---
+++ move_zeros.cpp
@@ -0,0 +1,11 @@
+#include "bits/stdc++.h"
+using namespace std;
+void moveZeroes(vector<int> &nums) {
+  for (int i = 0; i < nums.size(); i++) {
+    for (int j = i + 1; j < nums.size(); j++) {
+      if (nums[i] == 0) {
+        swap(nums[i], nums[j]);
+      }
+    }
+  }
+}
