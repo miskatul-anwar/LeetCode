f0b227a354f9c519cf5b53d279c21ab55b8b78d4
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-04-07 09:31:40 +0600

    Sun Apr  7 09:31:40 AM +06 2024

---
+++ reverse_bits.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  uint32_t reverseBits(uint32_t n) {
+    uint32_t ans = 0;
+
+    for (int i = 0; i < 32; ++i)
+      if (n >> i & 1) {
+        ans |= 1 << 31 - i;
+      }
+
+    return ans;
+  }
+};
