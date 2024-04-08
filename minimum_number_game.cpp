9e600bbb2ca89ffa0b2a9cf2074878826f113ddd
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-04-08 15:05:59 +0600

    Mon Apr  8 03:05:59 PM +06 2024

---
+++ minimum_number_game.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  vector<int> numberGame(vector<int> &nums) {
+    ranges::sort(nums);
+    for (int i = 0; i < nums.size(); i += 2)
+      swap(nums[i], nums[i + 1]);
+    return nums;
+  }
+};
