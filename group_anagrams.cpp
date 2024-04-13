8615f6cd5353256c6a236470ace1d71e9c23a03d
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-04-13 22:54:22 +0600

    Sat Apr 13 10:54:22 PM +06 2024

---
+++ group_anagrams.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<string>> groupAnagrams(vector<string> &strs) {
+  vector<vector<string>> ans;
+  unordered_map<string, vector<string>> keyToAnagrams;
+
+  for (const string &str : strs) {
+    string key = str;
+    ranges::sort(key);
+    keyToAnagrams[key].push_back(str);
+  }
+
+  for (const auto &[_, anagrams] : keyToAnagrams)
+    ans.push_back(anagrams);
+
+  return ans;
+}
