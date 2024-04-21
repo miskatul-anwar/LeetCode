90e4b21e74829ab1bb82cd1d090dba9b6760b7f7
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-04-21 19:15:41 +0600

    latest commit

---
+++ split_the_array.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  bool isPossibleToSplit(vector<int> &nums) {
+    constexpr int kMax = 100;
+    vector<int> count(kMax + 1);
+
+    for (const int num : nums)
+      ++count[num];
+
+    return ranges::all_of(count, [](int freq) { return freq <= 2; });
+  }
+};
